82bb8570101a3f60155aeead15ba110b03d71b81
EdenRize <edenrize@gmail.com>
2023-12-23 12:50:33 +0200

    Delete css files

--- cmps/ProgressBar.jsx
+++ cmps/ProgressBar.jsx
@@ -1,8 +1,6 @@
 export function ProgressBar({ todos }) {
-  console.log('todos', todos)
   const completedTodos = todos.filter((todo) => todo.doneAt)
   const percentCompleted = (completedTodos.length / todos.length) * 100
-  console.log('percentCompleted', percentCompleted)
   const progressBarStyle = {
     width: `${percentCompleted}%`,
   }
